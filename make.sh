33ace97f4498a1e4d9c733a0673d2f14e13fbe44
hato6502 <erisuke12345@yahoo.co.jp>
2019-04-06 01:10:18 +0900

    blade を PHP にコンパイルすることに成功しました。

---
+++ make.sh
@@ -0,0 +1,1 @@
+./blade test.blade.php && cat header.php test.php | php-cgi -q > test.html
---
+++ test.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php echo $__env->make('component', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</body>
+</html>
